8a0b34d6cca1a047a9c4c848b4903e9c12b47d5c
micha <michael.grossman066@qmail.cuny.edu>
2021-01-02 01:21:11 -0500

    Created a filter for file selection, and added a prompt for zip code with a regex. Will make a new commit with comments shortly

--- FileMenuHandler.java
+++ FileMenuHandler.java
@@ -3,13 +3,22 @@ package hackathon2021;
 import java.awt.event.*;
 import java.io.File;
 import javax.swing.*;
+import java.util.regex.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 
 public class FileMenuHandler implements ActionListener{
+
+	public class IllegalFileException extends IllegalArgumentException{
+		public IllegalFileException(String message){
+			super(message);
+		}
+	}
 	
 	Window myframe;
-	
+
 	FileMenuHandler (Window frame){			
-		myframe = frame;	
+		myframe = frame;
 	}
 	
 	/**
@@ -19,7 +28,11 @@ public class FileMenuHandler implements ActionListener{
 	public void actionPerformed(ActionEvent ae) {	
 		String eventName = ae.getActionCommand();
 		if(eventName.equals("Open")) {
-			openFile();
+			try {
+				openFile();
+			}catch(IllegalFileException e){
+				JOptionPane.showMessageDialog(null,e.getMessage());
+			}
 		}
 		else if(eventName.equals("Quit")) {
 			System.exit(0);
@@ -31,13 +44,28 @@ public class FileMenuHandler implements ActionListener{
 	 */
 	private void openFile()	{
 		JFileChooser chooser = new JFileChooser();
-		chooser.showOpenDialog(null);
-		if(chooser.getSelectedFile() != null)
-			readSource(chooser.getSelectedFile());
-	}
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG and JPEG FILE TYPES", "jpeg", "jpg");
+		chooser.setFileFilter(filter);
+		int retVal = chooser.showOpenDialog(null);
+		if(retVal == JFileChooser.APPROVE_OPTION)
+			myframe.image = chooser.getSelectedFile();
+		else if (retVal == JFileChooser.ERROR_OPTION)
+			throw new IllegalFileException("File Error, ERROR_OPTION Flagged");
+		String zip = JOptionPane.showInputDialog(null,"Please enter the zip code for this location");
+		Pattern p;
+		Matcher m;
+		String zipPattern = "^(\\d{5})(-\\d{4})?$";
+		p = Pattern.compile(zipPattern);
+		m = p.matcher(zip);
+		while(!m.matches() && !zip.isEmpty()){
+			zip = JOptionPane.showInputDialog(null,"Invalid Zip, Please enter the zip code for this location, blank for" +
+					" exit");
+			m = p.matcher(zip);
+		}
+		if(!zip.isEmpty()){
+			myframe.zipCode = zip;
+		}
 
-	private void readSource(File chosenFile) {
-		//Checking the chosen file
 	}
 	
 }
--- Window.java
+++ Window.java
@@ -1,13 +1,23 @@
 package hackathon2021;
 import javax.swing.*;
+import java.io.File;
+import java.awt.*;
 
 @SuppressWarnings("serial")
 public class Window extends JFrame{
+
+	final static String INTRO_ID = "intro";
+	final static String DISPLAY_ID = "display";
+	protected File image;
+	protected String zipCode;
+
 	/**
 	 * Builds the base of the program's window 
 	 */
 	public Window(){
 		super();
+		image = null;
+		zipCode = null;
 		this.setSize(900, 500);
 		this.setTitle("Fire Detection System FDS");
 		this.setDefaultCloseOperation(Window.EXIT_ON_CLOSE); //makes sure to close the window properly when clicking on 'x'
@@ -15,6 +25,9 @@ public class Window extends JFrame{
 		JMenuBar bar = new JMenuBar();
 		createFileMenu(bar); //calls the method below
 		this.setJMenuBar(bar); //put the menu bar on the window
+
+		createCardLayout(this.getContentPane());
+
 		this.setVisible(true);
 	}
 	
@@ -30,4 +43,27 @@ public class Window extends JFrame{
 		bar.add(file); //adds File to the menu bar
 	}
 
+	private void createCardLayout(Container pane){
+		JPanel introCard = new JPanel(); //card used to display the introdctory instructions
+		JPanel displayCard = new JPanel(); //card used to display processed data and imaging
+		JPanel cards = new JPanel(new CardLayout()); //panel used to hold card in a card layout.
+
+		//instructional JLabel for user, adds to intro card
+		JLabel introInstructions = new JLabel("<html><h2>Fire Detection and Prediction System</h2><hr>" +
+				"<ol><li>Select File</li><li>Click Open</li><li>Select the image file you wish to detect and " +
+				"predict for</li><li>Enter the Zip code and cardinal direction that the top of the image is facing</li>" +
+				"</ol></html>");
+		introCard.add(introInstructions);
+
+		//placeholder text to be overwritten once a file is selected, added to display card
+		JLabel displayCardHolder = new JLabel("This is a placeholder text. No data has been received yet");
+		displayCard.add(displayCardHolder);
+
+		//adding the intro and display to the holding panel using INTRO_ID and DISPLAY_ID as keys
+		cards.add(introCard, INTRO_ID);
+		cards.add(displayCard, DISPLAY_ID);
+
+
+		pane.add(cards, BorderLayout.CENTER);
+	}
 }
