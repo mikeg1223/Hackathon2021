add31cf5d41bc081c19370cb3a9d673806525eb2
Gabriel Atkin <gcdatkin@gmail.com>
2021-01-13 21:40:38 +0000

    Add python directory

---
+++ python/fire_detection.py
@@ -0,0 +1,25 @@
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+
+# Enter model filepath
+path_to_model = './fire_detector.h5'
+
+fire_detector = tf.keras.models.load_model(path_to_model)
+
+image_path = input("Enter filepath to an image: ")
+image = tf.expand_dims(
+    tf.keras.preprocessing.image.smart_resize(
+        tf.convert_to_tensor(
+            np.array(
+                Image.open(image_path)
+            )
+        ), size=(224, 224)
+    ), axis=0)
+
+prediction = fire_detector.predict(image)
+
+if int(prediction) == 1:
+    print("FIRE")
+else:
+    print("NO FIRE")
